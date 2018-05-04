fb5f9801f1b0493bce8cf8956304ff15da3fcb7f
anon <foo@foo.foo>
2018-05-04 11:19:40 -0700

    updated a bit

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"os"
 	"os/signal"
@@ -12,7 +13,8 @@ import (
 )
 
 var (
-	token string
+	token    string
+	pubgTime string
 )
 
 func init() {
@@ -39,9 +41,26 @@ func main() {
 
 func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if m.Author.ID == s.State.User.ID {
+		// ignore messages from self
 		return
 	}
-	if strings.Contains(m.Content, "!jeff") {
-		s.ChannelMessageSend(m.ChannelID, "Who's the sexiest man alive?")
+	// Store all commands here...
+	// Break up the message so that we are only seeing if it starts with
+
+	fmt.Println(m.Content)
+	fmt.Println(len(pubgTime))
+	switch dCmd := strings.Split(m.Content, " ")[0]; dCmd {
+	case "!pubg":
+		if len(pubgTime) > 0 {
+			s.ChannelMessageSend(m.ChannelID, pubgTime)
+		} else {
+			s.ChannelMessageSend(m.ChannelID, "No game scheduled...")
+		}
+	case "!schedule":
+		switch gName := strings.Split(m.Content, " ")[1]; gName {
+		case "pubg":
+			// Now we have to strip out the first two parts of the string:
+			pubgTime = strings.Replace(m.Content, "!schedule pubg ", "", -1)
+		}
 	}
 }
